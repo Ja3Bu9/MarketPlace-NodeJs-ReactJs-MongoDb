9a4aa53d4c096327f1913daaeb7d1a43a6c09ff5
Ja3Bu9 <ab.banana@hotmail.fr>
2021-03-22 03:28:53 +0100

    product

--- BackEnd/app/controllers/produit.controller.js
+++ BackEnd/app/controllers/produit.controller.js
@@ -4,6 +4,8 @@ const Vendeur = require('../models/model.vendeur');
 const util = require("util");
 const multer = require("multer");
 const maxSize = 2 * 1024 * 1024;
+var xss = require('xss');
+
 
 
 exports.addProduit = async (req, res) => {
@@ -15,7 +17,6 @@ exports.addProduit = async (req, res) => {
         },
         filename: (req, file, cb) => {
           file.originalname = Date.now() +'-'+ file.originalname
-          console.log(file.originalname);
           cb(null, file.originalname);
         },
       });
@@ -29,25 +30,31 @@ exports.addProduit = async (req, res) => {
     
       let uploadFileMiddleware = util.promisify(uploadFile);
 
-      try {
+    try {
+       
+
         await uploadFileMiddleware(req, res);
+       
+       
+            if (req.file == undefined) {
+                return res.status(400).send({ message: "Please upload a file!" });
+                 }
+        
 
-        if (req.file == undefined) {
-            return res.status(400).send({ message: "Please upload a file!" });
-             }
-
-             let produitPush = new Produit({
-                nom: req.body.nom,
-                prix: req.body.prix,
-                quantite: req.body.quantite,
-                photo: req.file.originalname,
-                idCategorie: req.body.idCategorie,
-                idVendeur: req.body.idVendeur
-               
-            })
             Vendeur.findOne({ _id: req.body.idVendeur })
-            .then((vendeur) => {
+            .then(async (vendeur) => {
                 if((vendeur.type == "Starter" && vendeur.produits < 10) || (vendeur.type == "Pro" && vendeur.produits < 50) || (vendeur.type == "Expert")){
+                    
+
+                    let produitPush = new Produit({
+                        nom: req.body.nom,
+                        prix: req.body.prix,
+                        quantite: req.body.quantite,
+                        photo: req.file.originalname,
+                        idCategorie: req.body.idCategorie,
+                        idVendeur: req.body.idVendeur
+                       
+                    })
 
                             produitPush.save()
                             .then(() => {
@@ -66,21 +73,14 @@ exports.addProduit = async (req, res) => {
 
                     }
                     if(vendeur.type == "Pro"){
+                        
                         res.json({notification : "please upgrade ur account to Expert"})
 
                     }
                 }
-                
-                
-
-
             })
             .catch((err) => res.json(err))
-
-            
-                
-
-      } catch (err) {
+    } catch (err) {
         if (err.code == "LIMIT_FILE_SIZE") {
             return res.status(500).send({
               message: "File size cannot be larger than 2MB!",
@@ -91,7 +91,7 @@ exports.addProduit = async (req, res) => {
             message: `Could not upload the file: ${req.file.originalname}. ${err}`,
           });
           
-      }
+    }
 
     
     
@@ -103,4 +103,58 @@ exports.getProduits = async (req, res) => {
     }).catch((err) => {
         res.status(500).json(err)
     })
+}
+
+exports.deleteProduit = async (req, res) => {
+    await Produit.remove({_id:req.params.idProduit}).then(()=>{
+        res.status(200).json("Product Deleted");
+    }).catch((err) => res.status(500).json(err));
+}
+
+exports.updateProduit = async (req, res) => {
+   
+    // XSS 
+    var nom = xss(req.body.nom, {
+        whiteList:          [],        // empty, means filter out all tags
+        stripIgnoreTag:     true,      // filter out all HTML not in the whilelist
+        stripIgnoreTagBody: ['script'] // the script tag is a special case, we need
+                                      // to filter out its content
+      });
+      var prix = xss(req.body.prix, {
+        whiteList:          [],        // empty, means filter out all tags
+        stripIgnoreTag:     true,      // filter out all HTML not in the whilelist
+        stripIgnoreTagBody: ['script'] // the script tag is a special case, we need
+                                      // to filter out its content
+      });
+      var quantite = xss(req.body.quantite, {
+        whiteList:          [],        // empty, means filter out all tags
+        stripIgnoreTag:     true,      // filter out all HTML not in the whilelist
+        stripIgnoreTagBody: ['script'] // the script tag is a special case, we need
+                                      // to filter out its content
+      });
+      var idCategorie = xss(req.body.idCategorie, {
+        whiteList:          [],        // empty, means filter out all tags
+        stripIgnoreTag:     true,      // filter out all HTML not in the whilelist
+        stripIgnoreTagBody: ['script'] // the script tag is a special case, we need
+                                      // to filter out its content
+      });
+    
+      // XSS
+    await Produit.updateOne({
+        _id:req.params.idProduit}, {$set :{
+            nom: nom,
+            prix: prix,
+            quantite: quantite,
+            idCategorie: idCategorie,
+           
+        }
+    }).then(() => {
+        res.status(200).json("Produit updated");
+    }).catch((err) => res.status(500).json(err))
+}
+
+exports.getProduit = async (req, res) => {
+    await Produit.findOne({_id:req.params.idProduit}).then((produit)=>{
+        res.status(200).json(produit);
+    }).catch((err) => res.status(500).json(err));
 }
--- BackEnd/app/routes/route.produit.js
+++ BackEnd/app/routes/route.produit.js
@@ -5,8 +5,10 @@ module.exports = app => {
 
     Router.route("/add").post(conntrollerproduit.addProduit);
     Router.route("/").get(conntrollerproduit.getProduits);
-    // Router.route("/update/:idCategorie").put(conntrollercategorie.updateCategorie);
-    // Router.route("/delete/:idCategorie").delete(conntrollercategorie.deleteCategorie);
+    Router.route("/:idProduit").get(conntrollerproduit.getProduit);
+
+    Router.route("/update/:idProduit").put(conntrollerproduit.updateProduit);
+    Router.route("/delete/:idProduit").delete(conntrollerproduit.deleteProduit);
 
 
     app.use("/produit", Router);
